03f57be9c85c7447ec26067f0b3a3ffd3589ba4d
LuciaAndrea <43391254+LuciaAndrea@users.noreply.github.com>
2018-12-05 12:03:54 +0100

    The fox arrives at the grapes

--- sketch.js
+++ sketch.js
@@ -1,11 +1,34 @@
 function preload(){
-  // put preload code here
+  character = loadImage("./assets/fox2.png");
+  fruit = loadImage("./assets/uva.png");
+
 }
+var mic;
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  mic = new p5.AudioIn();
+  mic.start();
 }
 
 function draw() {
-  // put drawing code here
+    background(255);
+
+fill('purple');
+noStroke();
+
+var myText = 'Sing aloud to let the fox reach the grapes';
+textFont('garamond');
+textAlign(CENTER);
+textSize(18);
+text(myText, windowWidth/2, windowHeight/9);
+imageMode(CENTER);
+rectMode(CENTER);
+console.log(-vol*1000);
+  fill('pink');
+  var vol = mic.getLevel();
+rect(windowWidth/2, windowHeight/8*6, windowWidth/70, -vol*1000)
+  fox = image(character, windowWidth/2*1.01, windowHeight/8*7, image.width, image.height);
+
+grapes = image(fruit, windowWidth/2, windowHeight/8*2, image.width, image.height);
 }
